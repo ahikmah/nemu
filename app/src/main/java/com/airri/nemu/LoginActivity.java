cf02724611f526c4be14056b2a49dfb934221405
Rizkychi <rizkynhae@gmail.com>
2019-12-27 12:28:14 +0700

    login activity

---
+++ app/src/main/java/com/airri/nemu/LoginActivity.java
@@ -0,0 +1,73 @@
+package com.airri.nemu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Collections;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // Membuat Kode Permintaan
+    private int RC_SIGN_IN = 1;
+
+    // Deklarasi variabel
+    Button btnLogin;
+    ProgressBar pbLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // inisialisasi variabel
+        btnLogin = findViewById(R.id.btn_login);
+        pbLogin = findViewById(R.id.pb_login);
+
+        // menonaktifkan loading
+        pbLogin.setVisibility(View.GONE);
+
+        // memberi aksi klik
+        btnLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_login) {
+            // Statement program untuk login/masuk
+            startActivityForResult(AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            //Memilih Provider atau Method masuk yang akan kita gunakan
+                            .setAvailableProviders(Collections.singletonList(new AuthUI.IdpConfig.GoogleBuilder().build()))
+                            .setIsSmartLockEnabled(false)
+                            .build(),
+                    RC_SIGN_IN);
+            // menampilkan loading
+            pbLogin.setVisibility(View.VISIBLE);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN adalah kode permintaan yang Anda berikan ke startActivityForResult, saat memulai masuknya arus.
+        if (requestCode == RC_SIGN_IN) {
+
+            //Berhasil masuk
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(LoginActivity.this, "Login Berhasil", Toast.LENGTH_SHORT).show();
+                finish();
+            }else {
+                //menghilangkan loading
+                pbLogin.setVisibility(View.GONE);
+                Toast.makeText(LoginActivity.this, "Login Dibatalkan", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
--- app/src/main/java/com/airri/nemu/MainActivity.java
+++ app/src/main/java/com/airri/nemu/MainActivity.java
@@ -1,8 +1,11 @@
 package com.airri.nemu;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -12,6 +15,9 @@ import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Deklarasi variabel
+    private FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,11 +26,21 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        // Mendapakan Instance Firebase Autentifikasi
+        auth = FirebaseAuth.getInstance();
+        // Memulai login
+        if(auth.getCurrentUser() == null){
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        } else {
+            //sek
+        }
     }
 
 }
